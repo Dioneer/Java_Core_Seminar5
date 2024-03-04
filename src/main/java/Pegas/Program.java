efbae20c1f305eaf133719aa52e1a96c12d96c16
Dioneer <gulkina_em@mail.ru>
2024-03-04 23:11:45 +0300

    IO

---
+++ src/main/java/Pegas/Program.java
@@ -0,0 +1,84 @@
+package Pegas;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Random;
+
+public class Program{
+    private static final Random random = new Random();
+    private static final int CHAR_BOUND_L=65;
+    private static final int CHAR_BOUND_H=90;
+    private static final String TO_SEARCH = "GeekBrains";
+
+    public static void main( String[] args ) throws IOException {
+
+        writeFileContents("sample1.txt", 35, 3);
+        writeFileContents("sample2.txt", 35, 2);
+        concat("sample1.txt", "sample2.txt", "concat.txt");
+        System.out.println(searchInFile("sample2.txt",TO_SEARCH));
+    }
+    private static String generateSymbols(int amount){
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < amount; i++) {
+            stringBuilder.append((char) random.nextInt(CHAR_BOUND_L, CHAR_BOUND_H+1));
+        }
+        return stringBuilder.toString();
+    }
+    private static void writeFileContents(String fileName, int length, int words) throws IOException {
+        File file = new File(fileName);
+        if(!file.exists()){
+            file.createNewFile();
+        }
+        try(BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(file))){
+            for (int i = 0; i < words; i++) {
+                bos.write(generateSymbols(length).getBytes(StandardCharsets.UTF_8));
+                if(random.nextInt(3)==0){
+                    bos.write(TO_SEARCH.getBytes(StandardCharsets.UTF_8));
+                    bos.write(generateSymbols(length).getBytes(StandardCharsets.UTF_8));
+                }
+            }
+        }
+    }
+    private static void concat(String file1, String file2, String fileOut) throws IOException {
+        try(BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(fileOut))){
+            try(BufferedInputStream bis1 = new BufferedInputStream(new FileInputStream(file1))){
+                byte[] arr = new byte[1024];
+                int r = bis1.read(arr);
+                while(r!=-1){
+                    bos.write(arr, 0 , r);
+                    r = bis1.read(arr);
+                }
+            }
+            try(BufferedInputStream bis2 = new BufferedInputStream(new FileInputStream(file2))){
+                byte[] arr = new byte[1024];
+                int r = bis2.read(arr);
+                while (r!=-1) {
+                    bos.write(arr, 0 , r);
+                    r = bis2.read(arr);
+                }
+            }
+        }
+    }
+    private static boolean searchInFile(String FileName, String search) throws IOException{
+        try(BufferedInputStream bis2 = new BufferedInputStream(new FileInputStream(FileName))){
+            byte[] arr = search.getBytes();
+            int r;
+            int i = 0;
+            while ((r=bis2.read())!=-1) {
+                if(r==arr[i]){
+                    i++;
+                }else{
+                    i=0;
+                    if(r==arr[i]) {
+                        i++;
+                        continue;
+                    }
+                }
+                if(i==arr.length){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
